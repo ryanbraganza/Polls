c96f03e8c2fd236293baf661a54da34aef89fb1b
Ryan Braganza <ryan.braganza@gmail.com>
2011-05-21 17:11:41 +1000

    add timeout to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :ldap_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :username, :password, :password_confirmation, :remember_me
